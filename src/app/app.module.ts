2788f36d36f0467e05c1b2762e8bd5921dbbdb1a
Milton Isaya <milton_isaiah@gmail.com>
2020-10-20 17:01:32 +0300

    material module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule} from './layouts/default/default.module';
 import { RouterModule } from '@angular/router';
+import { AngularMaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { RouterModule } from '@angular/router';
     AppRoutingModule,
     BrowserAnimationsModule,
     RouterModule,
-    DefaultModule
+    DefaultModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
