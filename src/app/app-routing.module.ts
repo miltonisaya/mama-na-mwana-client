4478e663b8a35a9dbfd77e0a755abf9e8d278c43
Milton Isaya <milton_isaiah@gmail.com>
2020-10-20 14:32:10 +0300

    routing error on children definition fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DashboardComponent} from './modules/dashboard/dashboard.component';
+import {DefaultComponent} from './layouts/default/default.component';
 
 const routes: Routes = [{
   path: '',
-  component: DashboardComponent
+  component: DefaultComponent,
+  children: [{
+    path: '',
+    component: DashboardComponent
+  }]
 }];
 
 @NgModule({
