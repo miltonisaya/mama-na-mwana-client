824b66cfb44a962348d9be5a1cff40f6d8ef1627
Milton Isaya <milton_isaiah@gmail.com>
2020-10-20 17:53:01 +0300

    flexlayoutmodule added

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -34,6 +34,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 @NgModule( {
@@ -71,7 +72,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
     MatSnackBarModule,
     MatTableModule,
     MatSortModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    FlexLayoutModule
 
   ],
   exports: [
@@ -90,7 +92,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
     MatChipsModule,
     MatTooltipModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    FlexLayoutModule
   ],
   providers: [
     MatDatepickerModule,
