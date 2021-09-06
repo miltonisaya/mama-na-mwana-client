7be1b7ab93a4138fc4f4510c7804c52559fc9228
Milton Isaya <milton_isaiah@gmail.com>
2021-09-06 16:11:40 +0300

    role test file

--- src/app/modules/roles/roles.service.ts
+++ src/app/modules/roles/roles.service.ts
@@ -1,16 +1,32 @@
 import { Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {map} from 'rxjs/operators';
+
+export const BASE_URL: string = environment.baseURL;
+export const RESOURCE_URL: string = 'api/v1/roles';
 
 @Injectable()
-
-export class usersService {
+export class rolesService {
+  private API_ENDPOINT = `${BASE_URL}/${RESOURCE_URL}`;
 
   constructor(private http: HttpClient) {}
 
-  private _url:string = 'http://localhost:8081/users';
-
-  getUsers(){
-    return this.http.get(this._url);
+  /**
+   * helper function to extract data since
+   * we are not using a type checker in the request
+   * @returns Observable
+   *
+   * @param res
+   */
+  private extractData(res: Response) {
+    const body = res;
+    return body || {};
   }
 
+  getRoles(): Observable<any> {
+    return this.http.get<any>(this.API_ENDPOINT).pipe(
+      map(this.extractData));
+  }
 }
