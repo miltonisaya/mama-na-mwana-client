50523ccf43435ca04ecfb71d4e773a77de9c9702
Milton Isaya <milton_isaiah@gmail.com>
2021-09-06 16:12:29 +0300

    users component added

--- src/app/modules/users/users.component.ts
+++ src/app/modules/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(){
+    let users = this.http.get("http://localhost:8081/api/v1/users").subscribe(res => {
+      console.log(res);
+      }
+    );
   }
 
 }
