6d6929215a627c51878d6bbe378cc1b612b55f5a
Milton Isaya <milton_isaiah@gmail.com>
2021-09-06 16:38:34 +0300

    delete dialogue added

--- src/app/modules/roles/roles.component.ts
+++ src/app/modules/roles/roles.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {rolesService} from './roles.service';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-users',
@@ -8,11 +9,17 @@ import {rolesService} from './roles.service';
   styleUrls: ['./roles.component.scss']
 })
 export class RolesComponent implements OnInit {
-  displayedColumns = ['name', 'description'];
+  displayedColumns = ['name', 'description', 'actions'];
   roles: any = [];
+  roleId: string;
+  @ViewChild('deleteDialog') deleteDialog: TemplateRef<any>;
+
   public dataSource = new MatTableDataSource<any>();
 
-  constructor(private roleService: rolesService) { }
+  constructor(
+    private roleService: rolesService,
+    private dialog: MatDialog
+    ) { }
 
   ngOnInit(): void {
     this.getRoles();
@@ -33,4 +40,22 @@ export class RolesComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  openDialog() {
+    console.log("Open dialog");
+  }
+
+  openDeleteDialog(id) {
+    this.roleId = id;
+    this.dialog.open(this.deleteDialog)
+      .afterClosed().subscribe(() => {
+      this.getRoles();
+    });
+  }
+
+  delete() {
+    this.roleService.delete(this.roleId)
+      .subscribe(response => {
+      });
+    this.dialog.closeAll();
+  }
 }
