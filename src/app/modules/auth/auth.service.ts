f98bb3860c4861976112d3ba52b12f577f98b1f2
Milton Isaya <milton_isaiah@gmail.com>
2021-09-07 17:28:41 +0300

    authentication service added

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  url = environment.baseURL + "/api/v1/authenticate";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  login(data):Observable<any>{
+    console.log(this.url);
+    // return this.url;
+    return this.http.post(this.url,data);
+  }
 
   getToken(){
     return "jwt-token"
