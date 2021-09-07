9fce15bf4f3e09ecd097ca8c1320d7cd2617e6d3
Milton Isaya <milton_isaiah@gmail.com>
2021-09-07 17:29:00 +0300

    login form added

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm(){
+    this.formGroup = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  loginProcess() {
+    if(this.formGroup.valid){
+      this.authService.login(this.formGroup.value).subscribe(result => {
+        if(result.success){
+          console.log(result);
+        } else {
+          console.log(result.message);
+        }
+      });
+    }
+  }
 }
