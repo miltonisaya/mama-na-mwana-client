0131a0125d28bcc4651708e9b99e2854a4c23e36
Milton Isaya <milton_isaiah@gmail.com>
2021-09-07 20:32:02 +0300

    login process implemented

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +10,12 @@ import {AuthService} from '../auth/auth.service';
 })
 export class LoginComponent implements OnInit {
   formGroup: FormGroup;
+  response;
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
     this.initForm();
@@ -24,14 +29,12 @@ export class LoginComponent implements OnInit {
   }
 
   loginProcess() {
-    if(this.formGroup.valid){
-      this.authService.login(this.formGroup.value).subscribe(result => {
-        if(result.success){
-          console.log(result);
-        } else {
-          console.log(result.message);
+    this.authService.login(this.formGroup.value)
+      .subscribe(response => {
+        console.log('the response', response)
+        if (response.data.user) {
+          this.router.navigate(['']);
         }
       });
-    }
   }
 }
