1dc46b49b8f6b6bc0422fcda533651268946b87a
Milton Isaya <milton_isaiah@gmail.com>
2021-09-07 20:31:38 +0300

    login method implemented

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
 import {environment} from '../../../environments/environment';
+import {Router} from '@angular/router';
+import {tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   url = environment.baseURL + "/api/v1/authenticate";
-  constructor(private http: HttpClient) { }
+  responseData;
 
-  login(data):Observable<any>{
-    console.log(this.url);
-    // return this.url;
-    return this.http.post(this.url,data);
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  login(data : any) {
+    return this.http.post<any>(this.url, data).pipe(
+      tap(response => {
+        const results = [];
+        const token = response.data.token;
+        const currentUser = response.data.user;
+        currentUser.token = token;
+        currentUser.menus = results;
+        localStorage.setItem("MNM_USER", JSON.stringify(currentUser));
+      }),
+    );
   }
 
   getToken(){
