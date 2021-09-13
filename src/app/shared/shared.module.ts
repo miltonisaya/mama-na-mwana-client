8188a13d109dcd08bc1268bda5bfb84c519d4db9
Milton Isaya <milton_isaiah@gmail.com>
2021-09-13 11:50:06 +0300

    area and card components added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,32 +1,40 @@
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './components/header/header.component';
 import {SidebarComponent} from './components/sidebar/sidebar.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {AngularMaterialModule} from '../material.module';
 import {RouterModule} from '@angular/router';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
-
+import {AreaComponent} from './widgets/area/area.component';
+import {HighchartsChartModule} from 'highcharts-angular';
+import {CardComponent} from './widgets/card/card.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    AreaComponent,
+    CardComponent
   ],
-    imports: [
-        CommonModule,
-        AngularMaterialModule,
-        RouterModule,
-        MatProgressBarModule
-    ],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    RouterModule,
+    MatProgressBarModule,
+    HighchartsChartModule
+  ],
   exports: [
     HeaderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    AreaComponent,
+    CardComponent
   ],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 
 })
-export class SharedModule { }
+export class SharedModule {
+}
