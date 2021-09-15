2d9a287c0930af54ea2395bff912d36eceba88cf
Milton Isaya <milton_isaiah@gmail.com>
2021-09-15 14:53:17 +0300

    dashboard reorganised

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -12,7 +12,7 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatDialogModule} from '@angular/material/dialog';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
-import {DashboardService} from '../../modules/dashboard.service';
+import {DashboardService} from '../../modules/dashboard/dashboard.service';
 
 
 @NgModule({
