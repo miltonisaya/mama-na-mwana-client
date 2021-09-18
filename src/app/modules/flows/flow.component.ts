43385b18e3efedbd37bef0f298243dfc8c515188
Milton Isaya <milton_isaiah@gmail.com>
2021-09-18 16:31:45 +0300

    flow keys fetched by flow id, table not rendered on empty

--- src/app/modules/flows/flow.component.ts
+++ src/app/modules/flows/flow.component.ts
@@ -1,16 +1,25 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {NotifierService} from '../notifications/notifier.service';
 import {FlowService} from './flow.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {Role} from '../roles/role';
 
 @Component({
   selector: 'app-users',
   templateUrl: './flow.component.html',
   styleUrls: ['./flow.component.scss']
 })
+
 export class FlowComponent implements OnInit {
   flows: any = [];
   selectedFlowId: any = null;
   flowKeys: any = [];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  displayedColumns: string[] = ["sno",'keyName', 'keyDescription','dataElement', 'actions'];
+  dataSource: MatTableDataSource<any>;
 
   constructor(
     private FlowService: FlowService,
@@ -52,9 +61,21 @@ export class FlowComponent implements OnInit {
     let id = event.value;
     return this.FlowService.getKeysByFlowId(id).subscribe((response: any) => {
       this.flowKeys = response.data;
+      this.dataSource = new MatTableDataSource<any>(this.flowKeys);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
       console.log(this.flowKeys);
     }, error => {
       this.notifierService.showNotification(error.message,'OK','error');
     })
+  }
+
+  mapDataElement(flowKeys) {
+    console.log("Mapping")
+  }
+
+  applyFilter($event: KeyboardEvent) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
