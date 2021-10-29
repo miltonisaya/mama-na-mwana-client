3967af2e1eaa93c12ba8316c323bac4da4c5e60b
Milton Isaya <milton_isaiah@gmail.com>
2021-10-29 18:56:30 +0300

    working on passing data from main component

--- src/app/modules/flows/modals/flow-key-dialog-component.ts
+++ src/app/modules/flows/modals/flow-key-dialog-component.ts
@@ -4,8 +4,8 @@ import {NotifierService} from '../../notifications/notifier.service';
 import {FlowKeyService} from '../flowkey.service';
 import {DataElementService} from '../../data-elements/dataElement.service';
 import {FormControl} from '@angular/forms';
-import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
+import {MatOptionSelectionChange} from '@angular/material/core';
 
 @Component({
   selector: 'app-flow-key-dialog',
@@ -14,11 +14,12 @@ import {map, startWith} from 'rxjs/operators';
 })
 
 export class FlowKeyDialogComponent implements OnInit {
-  dataElements : any;
+  dataElements: any;
   filteredOptions: any;
-  options: any;
   selectedDataElement: any;
   autoFilter: any;
+
+  myControl = new FormControl();
 
   constructor(
     public flowKeyService: FlowKeyService,
@@ -26,20 +27,28 @@ export class FlowKeyDialogComponent implements OnInit {
     public notifierService: NotifierService,
     public dataElementService: DataElementService,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.getDataElements();
+
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => typeof value === 'string' ? value : value.name),
+        map(name => name ? this._filter(name) : this.dataElements)
+      );
   }
 
-  getDataElements(){
+  getDataElements() {
     let params = {
-      pageSize : 1000
+      pageSize: 1000
     };
     return this.dataElementService.getDataElements(params).subscribe((response: any) => {
       this.dataElements = response.data.content;
     }, error => {
-      this.notifierService.showNotification(error.message,'OK','error');
+      this.notifierService.showNotification(error.message, 'OK', 'error');
       console.log(error);
     });
   }
@@ -48,7 +57,7 @@ export class FlowKeyDialogComponent implements OnInit {
     if (this.flowKeyService.form.valid) {
       this.flowKeyService.updateFlowKey(this.flowKeyService.form.value)
         .subscribe(response => {
-          this.notifierService.showNotification(response.message,'OK', 'success');
+          this.notifierService.showNotification(response.message, 'OK', 'success');
           this.onClose();
         });
     }
@@ -62,18 +71,29 @@ export class FlowKeyDialogComponent implements OnInit {
 
   mapDataElement() {
     let data = {
-      dataElementId : this.selectedDataElement,
+      dataElementId: this.selectedDataElement,
       rapidProFlowKeyId: this.data.id
     };
 
     this.flowKeyService.mapDataElement(data).subscribe((response: any) => {
-      if(response.status == '200'){
-        this.notifierService.showNotification(response.message,'OK','success');
+      if (response.status == '200') {
+        this.notifierService.showNotification(response.message, 'OK', 'success');
       }
     }, error => {
-      this.notifierService.showNotification(error,'OK','error');
+      this.notifierService.showNotification(error, 'OK', 'error');
       console.log(error);
     });
     this.dialogRef.close();
   }
+
+  displayFn(dataElement: any): string {
+    this.selectedDataElement = dataElement.id;
+    return dataElement && dataElement.name ? dataElement.name : '';
+  }
+
+  private _filter(name: string): any {
+    const filterValue = name.toLowerCase();
+    return this.dataElements.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
 }
+
