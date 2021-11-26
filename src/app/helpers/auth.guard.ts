96e7a38721baf4100ac64f1bbc7c828d32547837
Milton Isaya <milton_isaiah@gmail.com>
2021-11-26 16:23:59 +0300

    user fetched from local storage

--- src/app/helpers/auth.guard.ts
+++ src/app/helpers/auth.guard.ts
@@ -1,16 +1,14 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
-import {AuthService} from '../modules/auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   constructor(
-    private router: Router,
-    private authenticationService: AuthService
+    private router: Router
   ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const currentUser = this.authenticationService.currentUserValue;
+    const currentUser = localStorage.getItem('MNM_USER');
     if (currentUser) {
       // logged in so return true
       console.log("The current user ==>", currentUser);
