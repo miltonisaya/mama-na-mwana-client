db6209c765a53179fea8366ea5c37200a3162a6f
Milton Isaya <milton_isaiah@gmail.com>
2021-11-26 15:58:05 +0300

    transactions added to dashboard

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {DashboardService} from './dashboard.service';
+import {MatTableDataSource} from '@angular/material/table';
+import {Contact} from '../contacts/Contact';
+import {Transaction} from './Transaction';
+import {TransactionsService} from '../transactions/transactions.service';
+import {Role} from '../roles/role';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {NotifierService} from '../notifications/notifier.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,13 +20,51 @@ export class DashboardComponent implements OnInit {
   pieChart = [];
   barChart = [];
 
-  constructor(private dashboardService: DashboardService) { }
+  transactions: any;
+  displayedColumns: string[] = ["sno",'id', 'dateProcessed','payload','status','actions'];
+  contacts: any = [];
+  userId: string;
+  dataSource: MatTableDataSource<Transaction>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+
+  constructor(
+    private dashboardService: DashboardService,
+    private transactionService: TransactionsService,
+    private notifierService: NotifierService
+  ) { }
 
   ngOnInit(): void {
     this.bigChart = this.dashboardService.bigChart();
     this.cards =this.dashboardService.cards();
     this.pieChart = this.dashboardService.pieChart();
     this.barChart = this.dashboardService.barChart();
+    this.getOutbox();
   }
 
+  /**
+   * This method returns the outgoing messages
+   */
+  getOutbox() {
+    return this.transactionService.getTransactions().subscribe((response: any) => {
+      this.transactions = response.data;
+      this.dataSource = new MatTableDataSource<Transaction>(this.transactions.content);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      console.log(this.transactions);
+    }, error => {
+      this.notifierService.showNotification(error.message,'OK','error');
+      console.log(error);
+    });
+  }
+
+  tyrAgain(id) {
+    console.log(id);
+  }
+
+  applyFilterTransactions(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
--- src/app/modules/transactions/transactions.service.ts
+++ src/app/modules/transactions/transactions.service.ts
@@ -6,25 +6,14 @@ import {catchError, map, tap} from 'rxjs/operators';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 export const BASE_URL: string = environment.baseURL;
-export const RESOURCE_URL: string = 'api/v1/users';
+export const RESOURCE_URL: string = 'api/v1/outgoing-transactions';
 
 @Injectable()
 
-export class UsersService {
+export class TransactionsService {
   private API_ENDPOINT = `${BASE_URL}/${RESOURCE_URL}`;
 
   constructor(private http: HttpClient) {}
-
-  form: FormGroup = new FormGroup({
-    id: new FormControl(''),
-    name: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required]),
-    phone: new FormControl('', [Validators.required]),
-    username: new FormControl('', [Validators.required]),
-    title: new FormControl('', [Validators.required]),
-    roles: new FormControl([]),
-  });
 
   /**
    * helper function to extract data since
@@ -39,55 +28,12 @@ export class UsersService {
   }
 
   /**
-   * Get all users
+   * Get all transactions
    * @param param
    */
-  getUsers(param?): Observable<any> {
+  getTransactions(param?): Observable<any> {
     return this.http.get<any>(this.API_ENDPOINT,{params: param}).pipe(
       map(this.extractData));
-  }
-
-  /**
-   * Delete user by id
-   * @param id
-   */
-  delete(id): Observable<any> {
-    console.log("Deleting user with id ",id);
-    return this.http.delete<any>(this.API_ENDPOINT+"/"+id).pipe(
-      map(this.extractData));``
-  }
-
-  /**
-   *
-   * @param data
-   */
-  populateForm (data){
-    this.form.setValue(data);
-  }
-
-  initializeFormGroup(){
-    return this.form.setValue({
-      id: '',
-      email: '',
-      name: '',
-      password: '',
-      phone: '',
-      username: '',
-      title: '',
-      roles:[]
-    });
-  }
-
-  /**
-   * @param role
-   */
-  createUser(user): Observable<any> {
-    console.log(user);
-    return this.http.post<any>(this.API_ENDPOINT, user)
-      // tslint:disable-next-line:no-shadowed-variable
-      .pipe(tap((response) => console.log(`Added user with name = ${user.name}`)),
-        catchError(this.handleError<any>('create objective'))
-      );
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
@@ -101,16 +47,4 @@ export class UsersService {
       return of(result as T);
     };
   }
-
-  updateUser(user): Observable<any> {
-    return this.http.put(this.API_ENDPOINT+"/"+user.id, user)
-      .pipe(tap(_ => console.log(`updated user with id=${user.id}`)),
-        catchError(this.handleError<any>('update user'))
-      );
-  }
-
-  compareObjects(o1, o2) {
-    return o1 && o2 && o1.id === o2.id;
-  }
-
 }
