c9ff86a1141c26c57cc87e5aaca251090c2e4695
Milton Isaya <miltonisaiah@gmail.com>
2023-03-11 12:51:07 +0300

    council auto-complete added

--- src/app/modules/organisation-units/modals/organisation-unit-dialog-component.ts
+++ src/app/modules/organisation-units/modals/organisation-unit-dialog-component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {MatDialogRef} from '@angular/material/dialog';
 import {NotifierService} from '../../notifications/notifier.service';
 import {OrganisationUnitService} from '../organisation-unit.service';
+import {FormControl, Validators} from "@angular/forms";
+import {map, startWith} from "rxjs/operators";
 
 @Component({
   selector: 'app-organisation-unit-dialog',
@@ -10,6 +12,10 @@ import {OrganisationUnitService} from '../organisation-unit.service';
 })
 
 export class OrganisationUnitDialogComponent implements OnInit {
+  myControl = new FormControl([Validators.required]);
+  councils: any;
+  selectedCouncil: any;
+  filteredOptions: any;
   constructor(
     public OrganisationUnitService: OrganisationUnitService,
     public dialogRef: MatDialogRef<OrganisationUnitDialogComponent>,
@@ -17,9 +23,44 @@ export class OrganisationUnitDialogComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.getCouncils();
+
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => typeof value === 'string' ? value : value.name),
+        map(name => name ? this._filter(name) : this.councils)
+      );
   }
 
+  private _filter(name: string): any {
+    const filterValue = name.toLowerCase();
+    return this.councils.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
+
+  displayFn(council: any): string {
+    this.selectedCouncil = council.id;
+    return council && council.name ? council.name : '';
+  }
+
+  getCouncils() {
+    let params = {
+      pageSize: 1000
+    };
+    return this.OrganisationUnitService.getCouncils(params).subscribe((response: any) => {
+      this.councils = response.data;
+    }, error => {
+      this.notifierService.showNotification(error.error.error, 'OK', 'error');
+      console.log(error);
+    });
+  }
+
+
   submitForm(data) {
+    this.OrganisationUnitService.form.patchValue({parentId: this.myControl.value.id})
+    // data.parentId = this.myControl.value.id;
+    console.log('Is valid =>',this.OrganisationUnitService.form.valid);
+
     if (this.OrganisationUnitService.form.valid) {
       if (this.OrganisationUnitService.form.get('id').value) {
         this.OrganisationUnitService.updateOrganisationUnit(this.OrganisationUnitService.form.value)
