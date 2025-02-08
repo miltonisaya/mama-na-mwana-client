55e445fecff3fc293731a73cfa89f5d546c2332a
Milton <miltonisaiah@gmail.com>
2025-02-08 11:58:48 +0300

    app routing corrected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,74 +18,100 @@ import {ReportComponent} from './components/reports/report.component';
 import {DatasetsComponent} from "./components/datasets/datasets.component";
 
 
-const routes: Routes = [{
-  path: '',
-  component: DefaultComponent,
-  canActivate: [AuthGuard],
-  children: [{
-    path: 'dashboard',
-    component: DashboardComponent,
-    canActivate: [AuthGuard],
-  }, {
-    path: 'users',
-    component: UsersComponent,
-    canActivate: [AuthGuard],
-  },
-    {
-      path: 'roles',
-      component: RolesComponent,
-      canActivate: [AuthGuard],
-    },
-    {
-      path: 'flows',
-      component: FlowComponent,
-      canActivate: [AuthGuard],
-    }, {
-      path: 'data-elements',
-      component: DataElementComponent,
-      canActivate: [AuthGuard],
-    }, {
-      path: 'contacts',
-      component: ContactsComponent,
-      canActivate: [AuthGuard],
-    }, {
-      path: 'organisation-units',
-      component: OrganisationUnitComponent,
-      canActivate: [AuthGuard],
-    }, {
-      path: 'programs',
-      component: ProgramComponent,
-      canActivate: [AuthGuard],
-    }, {
-      path: 'datasets',
-      component: DatasetsComponent,
-      canActivate: [AuthGuard],
-    }, {
-      path: 'profile',
-      component: PasswordResetComponent,
-      canActivate: [AuthGuard],
-    },
-    {
-      path: 'authorities',
-      component: AuthorityComponent,
-      canActivate: [AuthGuard]
-    },
-    {
-      path: 'manage-menus',
-      component: MenuComponent,
-      canActivate: [AuthGuard]
-    },
-    {
-      path: 'manage-reports',
-      component: ReportComponent,
-      canActivate: [AuthGuard]
-    }
-  ]
-},
+// const routes: Routes = [{
+//   path: '',
+//   component: DefaultComponent,
+//   canActivate: [AuthGuard],
+//   children: [{
+//     path: 'dashboard',
+//     component: DashboardComponent,
+//     canActivate: [AuthGuard],
+//   }, {
+//     path: 'users',
+//     component: UsersComponent,
+//     canActivate: [AuthGuard],
+//   },
+//     {
+//       path: 'roles',
+//       component: RolesComponent,
+//       canActivate: [AuthGuard],
+//     },
+//     {
+//       path: 'flows',
+//       component: FlowComponent,
+//       canActivate: [AuthGuard],
+//     }, {
+//       path: 'data-elements',
+//       component: DataElementComponent,
+//       canActivate: [AuthGuard],
+//     }, {
+//       path: 'contacts',
+//       component: ContactsComponent,
+//       canActivate: [AuthGuard],
+//     }, {
+//       path: 'organisation-units',
+//       component: OrganisationUnitComponent,
+//       canActivate: [AuthGuard],
+//     }, {
+//       path: 'programs',
+//       component: ProgramComponent,
+//       canActivate: [AuthGuard],
+//     }, {
+//       path: 'datasets',
+//       component: DatasetsComponent,
+//       canActivate: [AuthGuard],
+//     }, {
+//       path: 'profile',
+//       component: PasswordResetComponent,
+//       canActivate: [AuthGuard],
+//     },
+//     {
+//       path: 'authorities',
+//       component: AuthorityComponent,
+//       canActivate: [AuthGuard]
+//     },
+//     {
+//       path: 'manage-menus',
+//       component: MenuComponent,
+//       canActivate: [AuthGuard]
+//     },
+//     {
+//       path: 'manage-reports',
+//       component: ReportComponent,
+//       canActivate: [AuthGuard]
+//     }
+//   ]
+// },
+//   {
+//     path: 'login',
+//     component: LoginComponent
+//   }];
+
+const routes: Routes = [
   {
-    path: 'login',
-    component: LoginComponent
-  }];
+    path: '',
+    component: DefaultComponent,
+    canActivate: [AuthGuard],
+    children: [
+      // Add default redirect for root path
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent }, // Removed redundant guard
+      { path: 'users', component: UsersComponent },
+      { path: 'roles', component: RolesComponent },
+      { path: 'flows', component: FlowComponent },
+      { path: 'data-elements', component: DataElementComponent },
+      { path: 'contacts', component: ContactsComponent },
+      { path: 'organisation-units', component: OrganisationUnitComponent },
+      { path: 'programs', component: ProgramComponent },
+      { path: 'datasets', component: DatasetsComponent },
+      { path: 'profile', component: PasswordResetComponent },
+      { path: 'authorities', component: AuthorityComponent },
+      { path: 'manage-menus', component: MenuComponent },
+      { path: 'manage-reports', component: ReportComponent },
+    ],
+  },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
