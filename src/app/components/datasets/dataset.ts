0de09f5e75d6fec21013026d60ffceacc5f6bbcc
Milton <miltonisaiah@gmail.com>
2025-02-08 11:59:35 +0300

    datasets interface defined

--- src/app/components/datasets/dataset.ts
+++ src/app/components/datasets/dataset.ts
@@ -1,6 +1,9 @@
+import {DataElement} from "../data-elements/dataElement";
+
 export interface Program {
   id: string;
   name: string;
   code: string;
   dhisUid: string;
+  dataElements:DataElement[];
 }
