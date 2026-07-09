a57bdf5a88e9175db9c7c986b1e05733c7f5db8f
Milton Isaya <miltonisaiah@gmail.com>
2026-07-09 22:06:03 +0300

    Remove FlexLayoutModule from data-elements NgModule and update Material imports
    FlexLayoutModule was removed from the project because @angular/flex-layout
    is incompatible with Angular 14+ (stale peer dep on @angular/cdk@^10).
    Also replaces any remaining MatLegacy* imports with their MDC equivalents
    as required by Angular Material v17+.

--- src/app/components/data-elements/dataElement.module.ts
+++ src/app/components/data-elements/dataElement.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FlexLayoutModule} from '@angular/flex-layout';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SharedModule} from 'src/app/shared/shared.module';
 import {AngularMaterialModule} from '../../material.module';
@@ -9,7 +8,6 @@ import {MatDialogModule} from '@angular/material/dialog';
 @NgModule({
   imports: [
     CommonModule,
-    FlexLayoutModule,
     AngularMaterialModule,
     ReactiveFormsModule,
     FormsModule,
