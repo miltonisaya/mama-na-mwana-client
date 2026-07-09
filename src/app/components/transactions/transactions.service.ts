6e81eab227d0daffe24a1dc29a2afa5851c00509
Milton Isaya <miltonisaiah@gmail.com>
2026-07-09 22:06:12 +0300

    Migrate transactions.service from HttpClientModule to inject(HttpClient) pattern for Angular 18

--- src/app/components/transactions/transactions.service.ts
+++ src/app/components/transactions/transactions.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {Observable, of} from 'rxjs';
 import {map} from 'rxjs/operators';
