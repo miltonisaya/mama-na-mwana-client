e52703589972970e7f293290dddeaf3b1ad2cd55
Milton Isaya <miltonisaiah@gmail.com>
2026-07-09 22:06:12 +0300

    Migrate datasets.service from HttpClientModule to inject(HttpClient) pattern for Angular 18

--- src/app/components/datasets/datasets.service.ts
+++ src/app/components/datasets/datasets.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {catchError, retry} from 'rxjs/operators';
 import {environment} from "../../../environments/environment";
