2b37808156f40744b3a882177b9249cb29270cce
Milton Isaya <miltonisaiah@gmail.com>
2026-07-09 22:06:12 +0300

    Migrate loading.interceptor from HttpClientModule to inject(HttpClient) pattern for Angular 18

--- src/app/interceptors/loading.interceptor.ts
+++ src/app/interceptors/loading.interceptor.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {LoaderService} from '../components/loader/loader.service';
 import {finalize} from 'rxjs/operators';
