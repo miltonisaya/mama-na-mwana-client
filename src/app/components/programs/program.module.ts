35aa5c3d387ab32b9f57cc3dd04beff182f6a324
Milton Isaya <miltonisaiah@gmail.com>
2026-07-09 21:24:24 +0300

    Replace PrimeNG PickListModule with Angular Material CheckboxModule, DividerModule, and ProgressBarModule in ProgramModule

--- src/app/components/programs/program.module.ts
+++ src/app/components/programs/program.module.ts
@@ -5,8 +5,10 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SharedModule} from 'src/app/shared/shared.module';
 import {AngularMaterialModule} from '../../material.module';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {DataElementProgramMappingDialogComponent} from "./modals/data-element-program-mapping-dialog-component";
-import {PickListModule} from "primeng/picklist";
 
 @NgModule({
   imports: [
@@ -17,7 +19,9 @@ import {PickListModule} from "primeng/picklist";
     FormsModule,
     SharedModule,
     MatDialogModule,
-    PickListModule
+    MatCheckboxModule,
+    MatDividerModule,
+    MatProgressBarModule
   ],
   declarations: [
     DataElementProgramMappingDialogComponent
