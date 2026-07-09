430c6bbdac6defed3ab24637d8e1a69801501a5a
Milton Isaya <miltonisaiah@gmail.com>
2026-07-09 22:06:03 +0300

    Remove FlexLayoutModule from menus NgModule and update Material imports
    FlexLayoutModule was removed from the project because @angular/flex-layout
    is incompatible with Angular 14+ (stale peer dep on @angular/cdk@^10).
    Also replaces any remaining MatLegacy* imports with their MDC equivalents
    as required by Angular Material v17+.

--- src/app/components/menus/menu.module.ts
+++ src/app/components/menus/menu.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FlexLayoutModule} from '@angular/flex-layout';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SharedModule} from 'src/app/shared/shared.module';
 import {AngularMaterialModule} from '../../material.module';
@@ -10,7 +9,6 @@ import {MenuDialogComponent} from "./modals/menu-dialog-component";
 @NgModule({
   imports: [
     CommonModule,
-    FlexLayoutModule,
     AngularMaterialModule,
     ReactiveFormsModule,
     FormsModule,
