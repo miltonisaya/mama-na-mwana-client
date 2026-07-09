1022e741cef6683dfe6bc11f00c6c03b0323a472
Milton Isaya <miltonisaiah@gmail.com>
2026-07-09 22:06:03 +0300

    Remove FlexLayoutModule from password-reset NgModule and update Material imports
    FlexLayoutModule was removed from the project because @angular/flex-layout
    is incompatible with Angular 14+ (stale peer dep on @angular/cdk@^10).
    Also replaces any remaining MatLegacy* imports with their MDC equivalents
    as required by Angular Material v17+.

--- src/app/components/password-reset/password-reset.module.ts
+++ src/app/components/password-reset/password-reset.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FlexLayoutModule} from '@angular/flex-layout';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SharedModule} from 'src/app/shared/shared.module';
 import {AngularMaterialModule} from '../../material.module';
@@ -12,7 +11,6 @@ import {MatDividerModule} from "@angular/material/divider";
 @NgModule({
   imports: [
     CommonModule,
-    FlexLayoutModule,
     AngularMaterialModule,
     ReactiveFormsModule,
     FormsModule,
