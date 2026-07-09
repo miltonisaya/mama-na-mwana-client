78cd35d182afd7348c82658001718d20a8f0b549
Milton Isaya <miltonisaiah@gmail.com>
2026-07-09 22:06:03 +0300

    Remove FlexLayoutModule from dashboard NgModule and update Material imports
    FlexLayoutModule was removed from the project because @angular/flex-layout
    is incompatible with Angular 14+ (stale peer dep on @angular/cdk@^10).
    Also replaces any remaining MatLegacy* imports with their MDC equivalents
    as required by Angular Material v17+.

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FlexLayoutModule} from '@angular/flex-layout';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SharedModule} from 'src/app/shared/shared.module';
 import {AngularMaterialModule} from '../../material.module';
@@ -11,24 +10,20 @@ import {DashboardComponent} from "./dashboard.component";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FlexLayoutModule,
-    AngularMaterialModule,
-    ReactiveFormsModule,
-    FormsModule,
-    SharedModule,
-    MatDialogModule,
-    PickListModule,
-    MatAutocompleteModule
-  ],
-  declarations: [
-    ReportParamsDialog
-  ],
-  entryComponents: [
-    DashboardComponent
-  ],
-  providers: []
+    imports: [
+        CommonModule,
+        AngularMaterialModule,
+        ReactiveFormsModule,
+        FormsModule,
+        SharedModule,
+        MatDialogModule,
+        PickListModule,
+        MatAutocompleteModule
+    ],
+    declarations: [
+        ReportParamsDialog
+    ],
+    providers: []
 })
 export class DashboardModule {
 }
