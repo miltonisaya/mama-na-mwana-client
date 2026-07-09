65a4ec0920041c73ebc8b9e7d95bc51dcad8560c
Milton Isaya <miltonisaiah@gmail.com>
2026-07-09 22:06:40 +0300

    Replace legacy Material types in users.component (MatLegacyTableDataSource, MatLegacyPaginator)
    Updates imports from @angular/material/legacy-* to the standard MDC-based modules
    as required by Angular Material v17 which removed all legacy component exports.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
