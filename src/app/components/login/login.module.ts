5723fdc12794e13300a61f5c2ecd76935dfd1e5a
Milton Isaya <miltonisaiah@gmail.com>
2026-07-16 12:01:35 +0300

    Consolidate LoginModule's Material imports
    This module hand-picked five individual Material modules
    (MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule,
    MatIconModule) instead of importing AngularMaterialModule like every
    other module in the app. Switched to AngularMaterialModule for
    consistency; all five are already exported by it.

--- src/app/components/login/login.module.ts
+++ src/app/components/login/login.module.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { AngularMaterialModule } from '../../material.module';
 import { LoginComponent } from './login.component';
 
 @NgModule({
@@ -15,11 +11,7 @@ import { LoginComponent } from './login.component';
     CommonModule,
     RouterModule,
     ReactiveFormsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
+    AngularMaterialModule,
   ],
 })
 export class LoginModule {}
