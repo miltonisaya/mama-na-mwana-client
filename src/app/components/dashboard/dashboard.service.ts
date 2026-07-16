f4bd15822f63b6062a08797d91aaa8b3e9b9fffd
Milton Isaya <miltonisaiah@gmail.com>
2026-07-16 12:40:04 +0300

    Add getContactsByAgeGroup to DashboardService
    Hits the new /stats/by-age-group backend endpoint, mirroring
    getContactsBySex.

--- src/app/components/dashboard/dashboard.service.ts
+++ src/app/components/dashboard/dashboard.service.ts
@@ -39,6 +39,10 @@ export class DashboardService {
     return this.http.get<any>(this.API_ENDPOINT + "/stats/by-sex", {params}).pipe(map(this.extractData));
   }
 
+  getContactsByAgeGroup(params?: any): Observable<any> {
+    return this.http.get<any>(this.API_ENDPOINT + "/stats/by-age-group", {params}).pipe(map(this.extractData));
+  }
+
   private extractData(res: Response) {
     return res || {};
   }
