43c9bf598f67eb1d1738e0682daa917df7871444
Milton Isaya <miltonisaiah@gmail.com>
2026-07-16 12:05:05 +0300

    Strip DefaultModule down to just the shell component
    DefaultModule is the eager sidenav+toolbar+router-outlet shell that
    wraps every route, but it also declared eight route components
    (DashboardComponent, UsersComponent, RolesComponent,
    DataElementComponent, OrganisationUnitComponent, ProgramComponent,
    AuthorityComponent, MenuComponent) and provided DashboardService.
    Since those components' routes are now lazy-loaded and each component
    has moved into its own feature module, DefaultModule only needs to
    declare DefaultComponent itself, plus whatever AngularMaterialModule
    directives (mat-drawer-container et al.) and SharedModule components
    (header/sidebar/footer) its own template uses.

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,55 +1,19 @@
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DefaultComponent} from './default.component';
-import {DashboardComponent} from '../../components/dashboard/dashboard.component';
-import {UsersComponent} from '../../components/users/users.component';
 import {SharedModule} from '../../shared/shared.module';
 import {RouterModule} from '@angular/router';
 import {AngularMaterialModule} from '../../material.module';
-import {RolesComponent} from '../../components/roles/roles.component';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatDialogModule} from '@angular/material/dialog';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
-import {DashboardService} from '../../components/dashboard/dashboard.service';
-import {DataElementComponent} from '../../components/data-elements/dataElement.component';
-import {OrganisationUnitComponent} from '../../components/organisation-units/organisation-unit.component';
-import {MatTreeModule} from '@angular/material/tree';
-import {ProgramComponent} from '../../components/programs/program.component';
-import {MatTabsModule} from "@angular/material/tabs";
-import {AuthorityComponent} from "../../components/authorities/authority.component";
-import {MenuComponent} from "../../components/menus/menu.component";
-
 
 @NgModule({
   declarations: [
-    DefaultComponent,
-    DashboardComponent,
-    UsersComponent,
-    RolesComponent,
-    DataElementComponent,
-    OrganisationUnitComponent,
-    ProgramComponent,
-    AuthorityComponent,
-    MenuComponent
+    DefaultComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule,
-    AngularMaterialModule,
-    MatProgressSpinnerModule,
-    MatProgressBarModule,
-    MatDialogModule,
-    ReactiveFormsModule,
-    FormsModule,
-    MatCardModule,
-    MatTreeModule,
-    MatTabsModule
-  ],
-  providers: [
-    DashboardService,
+    AngularMaterialModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
