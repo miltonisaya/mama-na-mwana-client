634dcf47e2a0cfb488b71e10eca890d9f0f062b0
Milton Isaya <miltonisaiah@gmail.com>
2026-07-16 12:01:35 +0300

    Consolidate LoginDialogModule's Material imports
    This module hand-picked five individual Material modules
    (MatDialogModule, MatFormFieldModule, MatInputModule, MatButtonModule,
    MatIconModule) instead of importing AngularMaterialModule like every
    other module in the app. Switched to AngularMaterialModule for
    consistency; all five are already exported by it.

--- src/app/components/login-dialog/login-dialog.module.ts
+++ src/app/components/login-dialog/login-dialog.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { AngularMaterialModule } from '../../material.module';
 import { LoginDialogComponent } from './login-dialog.component';
 
 @NgModule({
@@ -13,11 +9,7 @@ import { LoginDialogComponent } from './login-dialog.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
+    AngularMaterialModule,
   ],
 })
 export class LoginDialogModule {}
